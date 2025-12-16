78e125671e2e1929613cf5cce7349fe1feb89862
Prathamesh Salunke <salunkep341@gmail.com>
2025-12-16 13:59:05 +0530

    try

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-//   baseURL: "https://skill-task-backend-production.up.railway.app/api",
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+   baseURL: "https://skill-task-backend-production.up.railway.app/api",
+ // baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 export default api;
